9f2c85c9fdaaa9d3b6c100991e58b0e912e799dd
Ijustwantadadgumusername <spencerfrancisjr@gmail.com>
2024-03-18 22:18:15 -0500

    bloop

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - prints the values in a doubly linked list and returns
+ * the number of nodes
+ * @head: the head of the list
+ *
+ * Return: the number of nodes in the list
+*/
+
+size_t print_dlistint(const dlistint_t *head)
+{
+	size_t count = 0;
+	const dlistint_t *temp = NULL;
+
+	if (head == NULL)
+		return (0);
+	temp = head;
+	if (head->prev != NULL)
+	{
+		while (temp->prev != NULL)
+		{
+			temp = temp->prev;
+		}
+	}
+	while (temp != NULL)
+	{
+		count++;
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+	return (count);
+}
